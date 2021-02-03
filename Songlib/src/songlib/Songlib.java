b43bb46ff16ff9f7caf7a691e0bc945598bac605
freedomzx <lejongoco@gmail.com>
2021-02-03 15:50:02 -0500

    added javafx stuff based on lecture3part1

---
+++ Songlib/src/songlib/Songlib.java
@@ -0,0 +1,17 @@
+package songlib;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Songlib extends Application {
+
+	public void start(Stage primaryStage) {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+}
